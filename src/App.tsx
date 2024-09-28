1339116697fc127189489a776ea83c94b15c8b32
root <root@DESKTOP-1M64K71>
2024-09-28 15:53:55 +0300

    syncing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Button from 'vite-react-component-library-starter';
+import { Button } from 'vite-react-component-library-starter';
 import './App.css'
 
 function App() {
